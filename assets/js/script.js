33c767eb56e46ce6d5bf60ce60a52cee82854b3d
Matteo Piffer <matteopiffer@Matteos-iMac.local>
2019-06-06 18:58:44 +0200

    vue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,35 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+    
+    var app = new Vue({
+        el: '#app',
+        data: {
+            product: 'Socks',
+            image: 'https://via.placeholder.com/150/0000FF/808080 ?Text=Digital.com',
+            href: '#',
+            inStock: true,
+            onSale: false,
+            details:['80% cotton',' 20% polyester'],
+            variants: [
+                {
+                  variantId: 2234,
+                  variantColor: 'green'    
+                },
+                {
+                  variantId: 2235,
+                  variantColor: 'blue'
+                }
+            ],
+            sizes:[35,36,37],
+            cart:0
+            
+        },
+        methods:{
+            addToCart(){
+                this.cart += 1
+            }
+        }
+    })
+  
+  
 });
